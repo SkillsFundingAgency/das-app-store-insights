7562088b718a0ce80898f9d5cafed11cc95937bb
Ali Reid <ali.reid@live.co.uk>
2026-08-25 10:49:35 +0100

    Sonarcloud WIP App Store service registration
    This change removes duplicated dependency setup from the Feedback and Usage Metrics Azure Functions startup code.

--- src/SFA.DAS.AppStoreInsights.Feedback.Functions/Program.cs
+++ src/SFA.DAS.AppStoreInsights.Feedback.Functions/Program.cs
@@ -1,17 +1,10 @@
-using System.Net;
-using System.Security.Authentication;
-using System.Security.Cryptography.X509Certificates;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using SFA.DAS.AppStoreInsights.Feedback.Functions.Configuration;
 using SFA.DAS.AppStoreInsights.Feedback.Functions.Extensions;
-using SFA.DAS.AppStoreInsights.Shared.Clients;
-using SFA.DAS.AppStoreInsights.Shared.Infrastructure;
-using SFA.DAS.AppStoreInsights.Shared.Repositories;
+using SFA.DAS.AppStoreInsights.Shared.Extensions;
 using System.Diagnostics.CodeAnalysis;
-
-namespace SFA.DAS.AppStoreInsights.Feedback.Functions;
 
 [ExcludeFromCodeCoverage]
 public partial class Program
@@ -20,39 +13,14 @@ public partial class Program
     {
         var host = new HostBuilder()
             .ConfigureFunctionsWorkerDefaults()
-            .ConfigureAppConfiguration(builder =>
-            {
-                builder.AddConfiguration();
-            })
+            .ConfigureAppConfiguration(builder => builder.AddConfiguration())
             .ConfigureServices((context, s) =>
             {
                 s
                     .AddOptions()
                     .Configure<ApplicationConfiguration>(context.Configuration.GetSection(nameof(ApplicationConfiguration)))
-                    .AddApplicationRegistrations();
-
-                // Add Managed Identity support
-                s.AddSingleton<IManagedIdentityTokenProvider, ManagedIdentityTokenProvider>();
-                s.AddSingleton<IConnectionFactory, SqlServerConnectionFactory>();
-
-                // Register the repository with the connection string from configuration
-                s.AddSingleton<IAppStoreRepository>(sp =>
-                {
-                    var config = sp.GetRequiredService<IConfiguration>();
-                    var connectionString = config["SqlConnectionString"]
-                        ?? throw new InvalidOperationException("SqlConnectionString not found");
-                    var connectionFactory = sp.GetRequiredService<IConnectionFactory>();
-                    return new SqlAppStoreRepository(connectionFactory, connectionString);
-                });
-
-                // Configure Apple client with TLS 1.2
-                s.AddHttpClient<IAppleStoreClient, AppleStoreClient>()
-                    .ConfigurePrimaryHttpMessageHandler(() => new HttpClientHandler
-                    {
-                        SslProtocols = SslProtocols.Tls12
-                    });
-
-                s.AddSingleton<IGooglePlayClient, GooglePlayClient>();
+                    .AddApplicationRegistrations()
+                    .AddAppStoreInsightsServices(context.Configuration);
             })
             .Build();
 
--- src/SFA.DAS.AppStoreInsights.UsageMetrics.Functions/Program.cs
+++ src/SFA.DAS.AppStoreInsights.UsageMetrics.Functions/Program.cs
@@ -1,15 +1,10 @@
-using System.Diagnostics.CodeAnalysis;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using SFA.DAS.AppStoreInsights.Functions.UsageMetrics.Configuration;
 using SFA.DAS.AppStoreInsights.Functions.UsageMetrics.Extensions;
-using SFA.DAS.AppStoreInsights.Shared.Clients;
-using SFA.DAS.AppStoreInsights.Shared.Infrastructure;
-using SFA.DAS.AppStoreInsights.Shared.Repositories;
-using System.Security.Authentication;
-
-namespace SFA.DAS.AppStoreInsights.Functions.UsageMetrics;
+using SFA.DAS.AppStoreInsights.Shared.Extensions;
+using System.Diagnostics.CodeAnalysis; 
 
 [ExcludeFromCodeCoverage]
 public partial class Program
@@ -18,40 +13,14 @@ public partial class Program
     {
         var host = new HostBuilder()
             .ConfigureFunctionsWorkerDefaults()
-            .ConfigureAppConfiguration(builder =>
-            {
-                builder.AddConfiguration();
-            })
+            .ConfigureAppConfiguration(builder => builder.AddConfiguration())
             .ConfigureServices((context, s) =>
             {
                 s
                     .AddOptions()
                     .Configure<ApplicationConfiguration>(context.Configuration.GetSection(nameof(ApplicationConfiguration)))
-                    .AddApplicationRegistrations();
-
-                // Add Managed Identity support
-                s.AddSingleton<IManagedIdentityTokenProvider, ManagedIdentityTokenProvider>();
-                s.AddSingleton<IConnectionFactory, SqlServerConnectionFactory>();
-
-                // Register the repository with the connection string from configuration
-                s.AddSingleton<IAppStoreRepository>(sp =>
-                {
-                    var config = sp.GetRequiredService<IConfiguration>();
-                    var connectionString = config["SqlConnectionString"]
-                        ?? throw new InvalidOperationException("SqlConnectionString not found");
-                    var connectionFactory = sp.GetRequiredService<IConnectionFactory>();
-                    return new SqlAppStoreRepository(connectionFactory, connectionString);
-                });
-
-                // Configure Apple client with TLS 1.2 support
-                s.AddHttpClient<IAppleStoreClient, AppleStoreClient>()
-                    .ConfigurePrimaryHttpMessageHandler(() => new HttpClientHandler
-                    {
-                        SslProtocols = SslProtocols.Tls12
-                    });
-
-
-                s.AddSingleton<IGooglePlayClient, GooglePlayClient>();
+                    .AddApplicationRegistrations()
+                    .AddAppStoreInsightsServices(context.Configuration);
             })
             .Build();
 
